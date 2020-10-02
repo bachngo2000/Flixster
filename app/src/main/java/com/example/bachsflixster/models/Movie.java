173feeebbb1f910f65edf034bd07d33e7732fa4f
bachngo2000 <Bach.X.NguyenNgo-1@ou.edu>
2020-10-02 16:18:46 -0500

    Flixster 2

--- app/src/main/java/com/example/bachsflixster/models/Movie.java
+++ app/src/main/java/com/example/bachsflixster/models/Movie.java
@@ -3,16 +3,24 @@ package com.example.bachsflixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overView;
+    double rating;
+
+    //empty constructor needed by the Parceler library
+    public Movie()  {
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
 
@@ -20,6 +28,8 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overView = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -44,5 +54,13 @@ public class Movie {
 
     public String getOverView() {
         return overView;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
